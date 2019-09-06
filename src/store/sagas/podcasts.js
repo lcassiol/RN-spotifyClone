262aed7ad3c723adb1100083505d2e775f831bab
Cassio C <lccassiol@gmail.com>
2019-09-05 22:14:20 -0300

    Add log to display error or success from podcasts request

--- src/store/sagas/podcasts.js
+++ src/store/sagas/podcasts.js
@@ -9,8 +9,10 @@ import PodcastsActions from '../ducks/podcasts';
 export function* load() {
   try {
     const {data} = yield call(axios.get, '/podcasts');
+    console.tron.log(['success', data]);
     yield put(PodcastsActions.loadSuccess(data));
   } catch (err) {
+    console.tron.log(['error', err]);
     yield put(PodcastsActions.loadFailure());
   }
 }
